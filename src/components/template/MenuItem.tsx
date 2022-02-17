17f5b9cf842fa0af85fde6a67a7ae63d04097a58
Felipe Santos <felipesantos92.dev@gmail.com>
2022-02-17 16:50:17 -0300

    logout button

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,27 +1,43 @@
 import Link from 'next/link'
 
 interface MenuItemProps {
-  url: string
   texto: string
   icone: any
+  url?: string
+  className?: string
+  onClick?: (evento: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
-  return(
-    <li className={`hover:bg-gray-100`}>
-      <Link href={props.url}>
-        <a className={`
+  function rendLink() {
+    return (
+      <a className={`
           flex flex-col justify-center items-center
-          h-16 w-20
+          h-16 w-20 text-gray-600
+          ${props.className}
         `}>
           {props.icone}
           <span className={`
-            text-xs text-gray-600
+            text-xs 
           `}>
             {props.texto}
           </span>
         </a>
-      </Link>
+    )
+  }
+
+  return(
+    <li onClick={props.onClick} className={`
+      hover:bg-gray-100
+      cursor-pointer
+      `}>
+      {props.url ? (
+        <Link href={props.url}>
+          {rendLink()}
+        </Link>
+      ) : (
+        rendLink()
+      )}
     </li>
   )
 }
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -3,7 +3,9 @@ import MenuItem from "./MenuItem"
 import Logo from "./Logo"
 export default function MenuLateral() {
   return (
-    <aside>
+    <aside className={`
+      flex flex-col
+    `}>
       <div className={`
         flex flex-col items-center justify-center
         bg-gradient-to-r from-red-500 to-red-800
@@ -12,12 +14,21 @@ export default function MenuLateral() {
         <Logo />
 
       </div>
-      <ul>
+      <ul className="flex-grow">
         <MenuItem url="/" texto="Dashboard" icone={DashIcon} />
         <MenuItem url="/ingressos" texto="Ingressos" icone={TicketIcon} />
         <MenuItem url="/validacao" texto="Validação" icone={ShieldIcon} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={ConfigIcon} />
-        <MenuItem url="/" texto="Sair" icone={LogoutIcon} />
+      </ul>
+
+      <ul className="">
+        <MenuItem texto="Sair" icone={LogoutIcon}
+          onClick={() => console.log('Logout')}
+          className={`
+            text-red-600
+            hover:bg-red-400 hover:text-white
+          `}  
+        />
       </ul>
     </aside>
   )
