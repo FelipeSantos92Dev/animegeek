6f7b25e45132cca5617afd62bcbda31798f08128
Felipe Santos <felipesantos92.dev@gmail.com>
2022-02-21 21:47:20 -0300

    profile and index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,35 @@ export default function Home() {
   return (
     <Layout titulo="Página Inicial" subtitulo="Template Admin">
       <h3>Conteúdo</h3>
+      <div className="s mt-4 flex justify-between space-x-4">
+        <div className="flex w-1/3 items-center justify-around rounded-xl bg-white shadow-lg">
+          <img src="https://i.imgur.com/VHc5SJE.png" alt="" />
+          <div className="text-center">
+            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-4xl">
+              534
+            </h1>
+            <span className="text-gray-500">Coaches</span>
+          </div>
+        </div>
+        <div className="flex w-1/3 items-center justify-around rounded-xl bg-white shadow-lg">
+          <img src="https://i.imgur.com/Qnmqkil.png" alt="" />
+          <div className="text-center">
+            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-4xl">
+              9.7k
+            </h1>
+            <span className="text-gray-500">Kids</span>
+          </div>
+        </div>
+        <div className="flex w-1/3 items-center justify-around rounded-xl bg-white shadow-lg">
+          <img src="https://i.imgur.com/dJeEVcO.png" alt="" />
+          <div className="text-center">
+            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-4xl">
+              50 M
+            </h1>
+            <span className="text-gray-500">Revenue</span>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -4,6 +4,53 @@ export default function Perfil() {
   return (
     <Layout titulo="Perfil do Usuário" subtitulo="Dados pessoais">
       <h3>Perfil</h3>
+
+      <div className="relative my-24 mt-4 w-full overflow-hidden rounded shadow-2xl">
+        <div className="grid  bg-white ">
+          <div className="col-span-12 h-full pb-12 md:col-span-10 md:border-l md:border-solid md:border-black md:border-opacity-25">
+            <div className="px-4 pt-4">
+              <form action="#" className="flex flex-col space-y-8">
+                <div>
+                  <h3 className="text-2xl font-semibold">Basic Information</h3>
+                  <hr />
+                </div>
+
+                <div className="form-item">
+                  <label className="text-xl ">Full Name</label>
+                  <input
+                    type="text"
+                    value="Antonia P. Howell"
+                    className="focus:shadow-outline mr-2 w-full appearance-none rounded py-1 px-2 text-black  text-opacity-50 shadow focus:border-blue-200 focus:outline-none"
+                    disabled
+                  />
+                </div>
+
+                <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+                  <div className="form-item w-full">
+                    <label className="text-xl ">Username</label>
+                    <input
+                      type="text"
+                      value="antonia"
+                      className="focus:shadow-outline mr-2 w-full appearance-none rounded py-1 px-2 text-black text-opacity-50 text-opacity-25 shadow focus:border-blue-200 focus:outline-none "
+                      disabled
+                    />
+                  </div>
+
+                  <div className="form-item w-full">
+                    <label className="text-xl ">Email</label>
+                    <input
+                      type="text"
+                      value="antoniaph@gmail.com"
+                      className="focus:shadow-outline mr-2 w-full appearance-none rounded py-1 px-2 text-black text-opacity-50 text-opacity-25 shadow focus:border-blue-200 focus:outline-none "
+                      disabled
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
