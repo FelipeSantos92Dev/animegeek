32fc6cfcf1c066a1b23c2eee200de0e7e62aeb6f
Felipe Santos <felipesantos92.dev@gmail.com>
2022-02-23 16:09:35 -0300

    dashboard responsive 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,32 @@ export default function Home() {
   return (
     <Layout titulo="Painel Administrativo" subtitulo="Dados gerais">
       <h3>Dashboard</h3>
-      <div className="s mt-4 flex justify-between space-x-4">
-        <div className="flex w-1/3 items-center justify-around rounded-xl bg-white shadow-lg">
+
+      <div className="mt-4 md:flex">
+        <div className="m-4 flex items-center justify-around rounded-xl bg-white shadow-lg lg:w-1/3">
           <img src="https://i.imgur.com/VHc5SJE.png" alt="" />
           <div className="text-center">
-            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-4xl">
+            <h1 className=" font-bold text-gray-800 sm:text-xl lg:text-2xl">
               7.980
             </h1>
             <span className="text-gray-500">Ingressos Vendidos</span>
           </div>
         </div>
-        <div className="flex w-1/3 items-center justify-around rounded-xl bg-white shadow-lg">
+
+        <div className="m-4 flex items-center justify-around rounded-xl bg-white shadow-lg lg:w-1/3">
           <img src="https://i.imgur.com/Qnmqkil.png" alt="" />
           <div className="text-center">
-            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-4xl">
+            <h1 className=" font-bold text-gray-800 sm:text-xl lg:text-2xl">
               7.240
             </h1>
             <span className="text-gray-500">Público Presente</span>
           </div>
         </div>
-        <div className="flex w-1/3 items-center justify-around rounded-xl bg-white shadow-lg">
+        <div className="m-4 flex items-center justify-around rounded-xl bg-white shadow-lg lg:w-1/3">
           <img src="https://i.imgur.com/dJeEVcO.png" alt="" />
           <div className="text-center">
-            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-4xl">
-              R$ 72.000,00
+            <h1 className=" font-bold text-gray-800 sm:text-2xl lg:text-2xl">
+              R$ 72.000
             </h1>
             <span className="text-gray-500">Renda Total</span>
           </div>
