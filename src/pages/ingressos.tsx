780de82e3c5d7f16b556271fd73c04a36287e725
Felipe Santos <felipesantos92.dev@gmail.com>
2022-02-23 13:44:23 -0300

    add swr

--- src/pages/ingressos.tsx
+++ src/pages/ingressos.tsx
@@ -1,12 +1,28 @@
 import Layout from '../components/template/Layout'
+import useSWR from 'swr'
+import api from '../../utils/api'
 
 export default function Ingressos() {
+  const ticket = 20227436
+  const { data, error } = useSWR(`/api/ticket/${ticket}`, api)
+
+  if (error) {
+    console.log(error)
+  }
+
+  if (data) {
+    console.log(data)
+  }
+
   return (
     <Layout
       titulo="Gerenciamento de Ingressos"
       subtitulo="Tipo e emissão de ingressos"
     >
       <h3>Ingressos AnimeGeek</h3>
+      <p>Código do ingresso: {data?.data.codigo}</p>
+      <p>Validade do ingresso: {data?.data.validade}</p>
+      <p>Autenticações permitidas: {data?.data.autenticacao}</p>
     </Layout>
   )
 }
