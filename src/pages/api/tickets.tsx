8e71bc542d2b654f3f461f0e8ae64cb74ab6db10
Felipe Santos <felipesantos92.dev@gmail.com>
2022-02-22 21:40:31 -0300

    create and find tickets

--- src/pages/api/tickets.tsx
+++ src/pages/api/tickets.tsx
@@ -6,8 +6,9 @@ interface ErrorResponseType {
 }
 
 interface SuccessResponseType {
-  codigo: number
+  codigo: string
   qrcode: string
+  validade: string
 }
 
 export default async (
@@ -15,22 +16,73 @@ export default async (
   res: NextApiResponse<ErrorResponseType | SuccessResponseType>
 ): Promise<void> => {
   if (req.method === 'POST') {
-    const { codigo, qrcode } = req.body
+    const { validade } = req.body
 
-    if (!codigo || !qrcode) {
+    if (validade === 'sabado') {
+      const { db } = await connect()
+
+      const response = await db.collection('tickets').insertOne({
+        codigo: '2022' + (Math.floor(Math.random() * 10000) + 1),
+        validade,
+        autenticacao: 1,
+        leitura: 0,
+        //qrcode,
+      })
+      res.status(201).json(response.ops[0])
+    } else if (validade === 'domingo') {
+      const { db } = await connect()
+
+      const response = await db.collection('tickets').insertOne({
+        codigo: '2022' + (Math.floor(Math.random() * 10000) + 1),
+        validade,
+        autenticacao: 1,
+        leitura: 0,
+        //qrcode,
+      })
+      res.status(201).json(response.ops[0])
+    } else if (validade === 'combo') {
+      const { db } = await connect()
+
+      const response = await db.collection('tickets').insertOne({
+        codigo: '2022' + (Math.floor(Math.random() * 10000) + 1),
+        validade,
+        autenticacao: 2,
+        leitura: 0,
+        //qrcode,
+      })
+      res.status(201).json(response.ops[0])
+    }
+
+    if (!validade) {
       res.status(400).json({ error: 'Parâmetros inválidos!' })
       return
     }
+    // const { db } = await connect()
+
+    // const response = await db.collection('tickets').insertOne({
+    //   codigo: '2022' + (Math.floor(Math.random() * 10000) + 1),
+    //   qrcode,
+    //   validade,
+    //   // autenticacao: 0,
+    //   // leitura: 1,
+    // })
+    // res.status(201).json(response.ops[0])
+  } else if (req.method === 'GET') {
+    const { codigo } = req.body
+
+    if (!codigo) {
+      res.status(400).json({ error: 'Código não enviado!' })
+    }
+
     const { db } = await connect()
 
-    const response = await db.collection('tickets').insertOne({
-      codigo,
-      qrcode,
-      // validade: 'sabado',
-      // autenticacao: 0,
-      // leitura: 1,
-    })
-    res.status(200).json(response.ops[0])
+    const response = await db.collection('tickets').findOne({ codigo })
+
+    if (!response) {
+      res.status(400).json({ error: 'Ingresso não encontrado!' })
+      return
+    }
+    res.status(200).json(response)
   } else {
     res.status(400).json({ error: 'Método de envio errado' })
   }
